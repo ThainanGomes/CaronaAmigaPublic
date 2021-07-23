a740fd974cfd6e3dc2cb045aa778ff2ab1acff36
thainan <thainangomez@gmail.com>
2021-07-23 03:31:30 -0400

    Ajustando bibliotecas e configurando Stack inicial

---
+++ src/stacks/mainStack.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Hello from '../telas/Hello';
+import Inicial from '../telas/Inicial';
+import Login from '../telas/Login';
+
+const Stack = createStackNavigator();
+
+export default () => (
+    <Stack.Navigator>
+        <Stack.Screen name="Hello" component={Hello} />
+        <Stack.Screen name="Inicial" component={Inicial} />
+        <Stack.Screen name="Login" component={Login} />        
+    </Stack.Navigator>
+);
